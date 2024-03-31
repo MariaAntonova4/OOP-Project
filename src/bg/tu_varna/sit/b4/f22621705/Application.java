8cc51e5b7b52a7945b949753b168ec2076421d91
MariaAntonova4 <maria.an.antonova@gmail.com>
2024-03-31 23:56:20 +0300

    Work on PBMFile

--- src/bg/tu_varna/sit/b4/f22621705/Application.java
+++ src/bg/tu_varna/sit/b4/f22621705/Application.java
@@ -1,34 +1,52 @@
 package bg.tu_varna.sit.b4.f22621705;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        //FileOpen fileOpen=new FileOpen();
-        //SaveInFile saveInFile=new SaveInFile();
-        //CloseFile closeFile=new CloseFile();
+
+        FileWriter fileWriter1=new FileWriter("File.txt");
         FileWriter fileWriter=new FileWriter("PBMFile.pbm");
         File file=new File("C:\\Users\\Asus\\Desktop\\OOP-PROJECT\\OOP-Project\\Files");
-        //fileOpen.readFile(file);
-        //P1
-        //6 10
-        char []array={ //p1,
-                6,10,
+        char []array={ 'P','1',
+                '6','9',
+                '0', '0', '0', '0', '1', '0',
+                '0', '0', '0', '0', '1', '0',
+                '0', '0', '0', '0', '1', '0',
+                '0', '0', '0', '0', '1', '0',
+                '0', '0', '0', '0', '1', '0',
+                '1', '0', '0', '0', '1', '0',
+                '0', '1', '1', '1', '0', '0',
+                '0', '0', '0', '0', '0', '0',
+                '0', '0', '0', '0', '0', '0',
+                //0, 0, 0, 0, 1, 0,
+                //0, 0, 0, 0, 1, 0,
+                //0, 0, 0, 0, 1, 0,
+                //1, 0, 0, 0, 1, 0,
+                //0, 1, 1, 1, 0, 0,
+                //0, 0, 0, 0, 0, 0,
+                //0, 0, 0, 0, 0, 0
+                 };
+        int []array2={6, 10,
                 0, 0, 0, 0, 1, 0,
                 0, 0, 0, 0, 1, 0,
+                0, 0, 0, 0 ,1 ,0,
                 0, 0, 0, 0, 1, 0,
+                0 ,0, 0, 0, 1, 0,
                 0, 0, 0, 0, 1, 0,
-                0, 0, 0, 0, 1, 0,
-                0, 0, 0, 0, 1, 0,
-                1, 0, 0, 0, 1, 0,
-                0, 1, 1, 1, 0, 0,
-                0, 0, 0, 0, 0, 0,
+                1, 0 ,0, 0, 1, 0,
+                0, 1 ,1, 1, 0, 0,
+                0, 0, 0 ,0 ,0 ,0,
                 0, 0, 0, 0, 0, 0};
-        fileWriter.write(array);
+        fileWriter.write(Arrays.toString(array));
+        System.out.println(array);
+        fileWriter1.write(array);
+        fileWriter1.close();
         fileWriter.close();
+        FileReader fileReader1=new FileReader("File.txt");
         FileReader fileReader=new FileReader("PBMFile.pbm");
         fileReader.close();
-        //saveInFile.saveFile(fileWriter,array);
-        //closeFile.closeFile(file);
+        fileReader1.close();
     }
 }
