1e869e3460179d73cf8b6c1cd3422003c3d95897
MariaAntonova4 <maria.an.antonova@gmail.com>
2024-06-22 17:05:30 +0300

    Save and Monochrome2

---
+++ src/bg/tu_varna/sit/b4/f22621705/save/SaveInPBMFile.java
@@ -0,0 +1,75 @@
+package bg.tu_varna.sit.b4.f22621705.save;
+import bg.tu_varna.sit.b4.f22621705.Menu;
+import bg.tu_varna.sit.b4.f22621705.open.OpenPBM;
+
+import java.io.*;
+import java.util.Scanner;
+
+import static bg.tu_varna.sit.b4.f22621705.save.SaveFile.filePath;
+
+public class SaveInPBMFile {
+    private StringBuilder format=new StringBuilder();
+
+    private int width;
+    private int height;
+    private StringBuilder data=new StringBuilder();
+
+    public StringBuilder getData() {
+        return data;
+    }
+
+    public void setData(StringBuilder data) {
+        this.data = data;
+    }
+
+    public StringBuilder getFormat() {
+        return format;
+    }
+
+    public void setFormat(StringBuilder format) {
+        this.format = format;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Menu saveFile(String fileName,StringBuilder stringBuilder) throws IOException {
+        File file=new File(filePath+fileName);
+        FileOutputStream fileWriter=new FileOutputStream(file);
+        OpenPBM openPBM=new OpenPBM();
+        openPBM.readFile(fileName);
+        fileWriter.write(openPBM.getFormat().charAt(0));
+        fileWriter.write(openPBM.getFormat().charAt(1));
+        fileWriter.write('\n');
+        fileWriter.write(openPBM.getHeight());
+        fileWriter.write(" ".getBytes());
+        fileWriter.write(openPBM.getWidth());
+        fileWriter.write('\n');
+        int dataLength=1;
+        while(dataLength<stringBuilder.length()){
+            if (stringBuilder.charAt(dataLength)=='\n'){
+                fileWriter.write('\n');}else {
+                fileWriter.write(stringBuilder.charAt(dataLength));
+            }
+            dataLength++;
+        }fileWriter.close();
+        return null;
+        }
+    }
+
+
+
+
